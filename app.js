28edd6ea71717ef4c74483c0b99d6de051bb180b
Mandylaaane <ljungamanda@gmail.com>
2024-09-20 14:54:35 +0200

    Finished math functions.

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-//1.CREATE FUNCTIONS for items; add, subtract, multiply, divide. Test in console.
-
 //GLOBAL VARIABLES
 let operator = '';
-let currentValue = '';
+let previousValue ='';
+let currentValue ='';
 
 document.addEventListener("DOMContentLoaded", function(){
     //STORE all components on HTML in JS
@@ -13,16 +12,87 @@ document.addEventListener("DOMContentLoaded", function(){
     let numbers = document.querySelectorAll(".number");
     let operators = document.querySelectorAll(".operator");
 
-    let currentDisplay = document.querySelector(".display");
+    let previousDisplay = document.querySelector(".previous");
+    let currentDisplay = document.querySelector(".current");
     
     numbers.forEach((number) => number.addEventListener("click", function(e){
         handleNumber(e.target.textContent)
         currentDisplay.textContent = currentValue;
     }))
 
+    operators.forEach((op) => op.addEventListener("click", function (e){
+        handleOperator(e.target.textContent)
+        previousDisplay.textContent = previousValue + " " + operator;
+        currentDisplay.textContent = currentValue;
+
+    }))
+
+    clear.addEventListener("click", function(){
+        previousValue = '';
+        currentValue = '';
+        operator = '';
+        previousDisplay.textContent = currentValue;
+        currentDisplay.textContent = currentValue;
+
+
+    })
+
+    equal.addEventListener("click", function(){
+        if(currentValue != '' && previousValue != ''){
+          calculate()
+            previousDisplay.textContent = '';
+            if(previousValue.length <= 5){
+              currentDisplay.textContent = previousValue;
+            } else{
+            currentDisplay.textContent = previousValue.slice(0,5) + "...";
+            }
+        }
+    })
+
+    decimal.addEventListener("click", function(){
+        addDecimal();
+      })
 })
 
 function handleNumber(num) {
-    currentValue += number;
+    if(currentValue.length <= 5){
+        currentValue += num;
+    }
+}
 
+function handleOperator(op){
+    operator = op;
+    previousValue = currentValue;
+    currentValue = '';
+
+}
+
+function calculate(){
+    previousValue = Number(previousValue);
+    currentValue = Number(currentValue);
+
+    if(operator === "+"){
+        previousValue += currentValue;
+    } else if(operator === "-"){
+        previousValue -= currentValue;
+    } else if(operator === "*"){
+        previousValue *= currentValue;
+    } else{
+        previousValue /= currentValue;
+    }
+
+    previousValue = roundNumber(previousValue);
+    previousValue = previousValue.toString();
+    currentValue = previousValue.toString();
+
+}
+
+function roundNumber(num){
+    return Math.round(num * 1000) / 1000;
+}
+
+function addDecimal(){
+    if(!currentValue.includes(".")){
+        currentValue += '.';
+    }
 }
